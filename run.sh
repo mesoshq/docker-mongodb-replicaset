c719dca0e23af41e5913c3623a8d3cf0f2819aef
tobilg <tobilg@gmail.com>
2016-09-13 13:37:12 +0200

    Create default value for $CONTAINER_PATH variable

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -m
 
-# Check if we find the HOST ip address from Marathon, otherwise find gateway ip address via route info
+# Check if we find the HOST ip address from the framework, otherwise find gateway ip address via route info
 if [ "$HOST" != "" ]; then
     ip=$HOST
 else
@@ -9,12 +9,20 @@ else
     ip=$(ip route get 8.8.8.8 | awk '{print $NF; exit}')
 fi
 
-# Check if we receive a PORT0 from Marathon
+# Check if we receive a PORT0 from the framework
 if [ "$PORT0" != "" ]; then
     port=$PORT0
 else
     # Use standard port
     port="27017"
+fi
+
+# Check if we receive a CONTAINER_PATH from the framework
+if [ "$CONTAINER_PATH" != "" ]; then
+    relative_path=$CONTAINER_PATH
+else
+    # Use standard relative path
+    relative_path="data"
 fi
 
 # Configure storage engine
@@ -42,13 +50,13 @@ if [ "$REPLICA_SET" != "" ]; then
 fi
 
 # Set data directory
-export DATA_PATH=$MESOS_SANDBOX/$CONTAINER_PATH/db
+export DATA_PATH=$MESOS_SANDBOX/$relative_path/db
 mkdir -p $DATA_PATH
 
 cmd="$cmd --dbpath $DATA_PATH"
 
 # Set log directory
-export LOG_PATH=$MESOS_SANDBOX/$CONTAINER_PATH/logs
+export LOG_PATH=$MESOS_SANDBOX/$relative_path/logs
 mkdir -p $LOG_PATH
 
 cmd="$cmd --logpath $LOG_PATH/mongodb.log"
