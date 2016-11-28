4c4e1b40df8f4546c5d497586da1df1159566a75
tobilg <tobilg@gmail.com>
2016-11-28 16:26:03 +0100

    Add init script for ReplicaSet

---
+++ init.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+set -m
+
+if [ "$ADD_DELAY" != "" ]; then
+    echo "No application name (APP_NAME) env var specified. Exiting!"
+    exit 1
+fi
+
+# Check if there's an env var for the ReplicaSet addition delay
+if [ "$ADD_DELAY" != "" ]; then
+    add_delay=$ADD_DELAY
+else
+    add_delay=15
+fi
+
+# Gather the MongoDB endpoints from the Marathon API like this: host1:27017,host2:27017 etc.
+MONGODB_ENDPOINTS=$(curl -H "Content-Type: application/json" http://master.mesos:8080/v2/apps/$APP_NAME/tasks | jq -r '.tasks | map([.host, (.ports[0]|tostring)] | join(":")) | join(",")')
+
+# Read the MongoDB endpoints as array
+IFS=',' read -ra ENDPOINTS <<< "$MONGODB_ENDPOINTS"
+
+echo "Using ${ENDPOINTS[0]} to initialize the ReplicaSet!"
+
+# Connect to the first endpoint and initilize the ReplicaSet
+mongo ${ENDPOINTS[0]} --eval "printjson(rs.initialize())"
+
+# Process the other nodes -> add to ReplicaSet
+for endpoint in "${ENDPOINTS[@]:1}"; do
+    echo "Adding endpoint $endpoint to the ReplicaSet"
+    mongo $endpoint --eval "rs.add('$endpoint')"
+    sleep $add_delay
+done
+
+# Check the ReplicaSet's status
+mongo ${ENDPOINTS[0]} --eval "printjson(rs.status())"
